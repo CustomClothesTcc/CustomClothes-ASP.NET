4554194bdb3815a8ac15dae221b348460e94457f
Miguelpds10 <10072006miguel@gmail.com>
2024-08-31 16:08:25 -0300

    Criando o Projeto Asp.Net da Custom Clothes

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomClothing.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
