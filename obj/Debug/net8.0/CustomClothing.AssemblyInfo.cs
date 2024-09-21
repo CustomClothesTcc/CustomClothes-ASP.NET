c8a3a7fae1fcbe92c22e89ce5cfac788f44a41e8
LuKLopes2007 <luklopes2007@gmail.com>
2024-09-21 15:54:11 -0300

    no carrosel de novo

--- obj/Debug/net8.0/CustomClothing.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomClothing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f2a066d01a24cccd7899c56340b1e75219eab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e23aced00b56a48d3675048658504d1be85f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
