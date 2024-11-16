31ccbb1ccbe5225da2576ff42e0bc492cd668f40
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-11-16 19:50:30 -0300

    Tela de personalizar alterada apenas com bootstrap

--- obj/Debug/net8.0/CustomClothing.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomClothing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f20a245b01339f7120e6c60ed4266f4ecbcff5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233cf6401bae0f20535e826266d13b89e8ee9fe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
