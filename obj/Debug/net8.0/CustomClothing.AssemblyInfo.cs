4ccae6784713b71b5b62753b2608830af6f6259c
KaiqueVfreitas <Kaique.vieira2502Eu@outlook.com>
2024-11-25 16:19:10 -0300

    Adição de arquivos auxiliares

--- obj/Debug/net8.0/CustomClothing.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomClothing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bccf07928be2577a84c1b6add17c35c4ad9697b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f514268a3d78c1aa28305a0463c901904d3a93d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomClothing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
